6cdc9c82a7178a3b236ad5a3046cd393ce7d6b0a
eduardouio <accxell_ax3c@hotmail.es>
2013-04-29 11:40:33 -0500

    envio de pruebA

--- appweb/views/welcome_message.php
+++ appweb/views/welcome_message.php
@@ -168,38 +168,19 @@
   <div class="span4">    
   <h3>Certificaciones</h3>
   <p>
-   <b>OHSAS 18001:2007</b><br>   
-    Certificado de seguridad industrial<br>
-    <a href="certificacion/ohsas18000/">Continuar...</a>
-  </p>          
-  <p>
-   <b>ISO 9001:2008</b><br>   
-    Certificado de calidad <br>
-    <a href=" certificacion/iso9001/">Continuar...</a>
+   conta
   </p>          
   </div><!--/span-->       
   <div class="span4">    
   <h3>Servicios</h3>
   <p>
-    </p><ul>
-    <li>Dewatering</li>
-    <li>Biorremediación</li>    
-    <li>Perforación de Pozos</li>
-    <li>Sistema De Tratamiento De Aguas</li>
-    <li>Manejo y tratamiento de cortes de perforación</li>    
-    <li><a href="servicios/">Continuar..</a></li>
-    </ul>
-  <p></p>          
+    sl
+    </p>          
   </div><!--/span-->       
   <div class="span4">    
   <h3>Contactos</h3>
   <p>
-  <b>Dirección: </b> Av. Cristóbal Colon 1133 y Av. Rio Amazonas Edificio Arista Piso 5 <b>Of:</b> 502
-  <b>Sudamerica-Ecuador</b><br>
-  <b>Telefonos: </b> 593 (2) 2563-733 593 (2) 2551-961 <b>Fax:</b> 593 (2) 2233-578
-  </p>
-  <p>
-  <b>Email: </b> <a class="btn btn-inverse" href="mailto:operaciones@sln-ec.com">operaciones@sln-ec.com </a>
+  ll
   </p>                    
   </div><!--/span-->       
   </div><!--/row-->    
