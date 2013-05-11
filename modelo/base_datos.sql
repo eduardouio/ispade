3d25d1d4c99cc914b5af17872a43c9d61ecbd272
eduardouio <accxell_ax3c@hotmail.es>
2013-05-10 19:01:17 -0500

    fin de la jornada en la oficina, avance en el modelo de la base de datos

--- modelo/base_datos.sql
+++ modelo/base_datos.sql
@@ -10,9 +10,16 @@
 -- --------------------------------------------------------------------------------
 
   -- -----------------------------------------------------
+  -- Base Datos
+  -- -----------------------------------------------------
+
+  CREATE TABLE liposerv_ispade;
+  USE liposerv_ispade;
+
+  -- -----------------------------------------------------
   -- Table `Liposerv_ispade`.`usuario`
   -- -----------------------------------------------------
-  CREATE  TABLE IF NOT EXISTS `liposerv_ispade`.`usuario` (
+  CREATE  TABLE IF NOT EXISTS `liposerv_ispade`.`user` (
     `id_usuario` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT ,
     `usuario` VARCHAR(40) NOT NULL ,
     `pass` VARCHAR(200) NOT NULL ,        
@@ -24,15 +31,46 @@
     PRIMARY KEY (`usuario`) ,
     UNIQUE INDEX `id_usuario_UNIQUE` (`id_usuario` ASC) 
     )
-    ENGINE = InnoDB
-    AUTO_INCREMENT = 1
-    COMMENT = 'Tabla de usuarios, encargados de ingresar a la base de datos';
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  COMMENT = 'Tabla de usuarios, encargados de ingresar a la base de datos';
 
+  -- -----------------------------------------------------
+  -- Table `Liposerv_ispade`.`Paginas`
+  -- -----------------------------------------------------    
+  CREATE TABLE IF NOT EXISTS `liposerv_ispade`.`page`(
+    `id_page` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,    
+    `title` VARCHAR(500) NOT NULL,
+    `keywords` VARCHAR(500) NOT NULL,
+    `create_date` DATETIME NOT NULL,
+    `last_update` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id_articulo)
+    )
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  COMMENT = 'Listado de paginas del sitio, lás paginas se almacenan en forma de registros';
 
-
-CREATE TABLE IF NOT EXISTS `liposerv_ispade`.`articulo`(
-id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-fecha DATETIME NOT NULL,
-last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-
-) ENGINE = InnoDB;
+  -- -----------------------------------------------------
+  -- Table `Liposerv_ispade`.`articulos`
+  -- -----------------------------------------------------  
+  CREATE TABLE IF NOT EXISTS `liposerv_ispade`.`article`(
+    `id_article` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,    
+    `id_page` SMALLINT UNSIGNED NOT NULL,    
+    `title` VARCHAR(500) NOT NULL,
+    `keywords` VARCHAR(500) NOT NULL,
+    `image` VARCHAR(300) NOT NULL,
+    `content` MEDIUMTEXT NOT NULL,
+    `counter` SMALLINT UNSIGNED NOT NULL,
+    `create_date` DATETIME NOT NULL,
+    `last_update` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id_articulo),
+    INDEX `fk_id_page_idx` (`id_page` ASC) ,
+    CONSTRAINT `fk_id_page`
+      FOREIGN KEY (`id_page` )
+      REFERENCES `liposerv_ispade`.`page` (`id_page` )
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE
+    )
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  COMMENT = 'Tabla encargada de manejar los articulos de la pagina, en imagen va el link de una imágen o un video';
