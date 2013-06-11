07bcb8359aa20eb8416f85b30684db24ab6aa5ad
eduardouio <eduardouio7@gmail.com>
2013-06-11 16:08:29 -0500

    se añade dos archivos para el manejo del login y logout, ademas se comenta dos funciones para el modelo estableciendo los metodos de obtencion y escritura de datos en las tablas

--- sitio/appweb/models/dbsitio.php
+++ sitio/appweb/models/dbsitio.php
@@ -27,7 +27,49 @@ class DbSitio extends CI_Model{
 		parent::__construct();
 	}
 
-	public function getRows($table,$condition){
+
+	
+	/**
+	* Realiza una consulta tipo SELECT a la Base de datos, se arma la consulta usando funciones
+	* para cadenas el objetivo es genar el SQL de la consulta a travez de parametros, existen
+	* parametros opcionales indentificados con el "=0", esto se evalua para no tomar en cuenta
+	* ese parametro al momento de armar la consulta.
+	*
+	* SELECT
+    * [ALL | DISTINCT | DISTINCTROW ]      
+    * [FROM table_references
+    * [WHERE where_condition]
+    * [GROUP BY {col_name | expr | position}
+    * [ORDER BY {col_name | expr | position}
+    *  [ASC | DESC], ...]
+    * [LIMIT {[offset,] row_count | row_count OFFSET offset}]
+	* 
+	* @param str $table => Contiene el nombre de la tabla a la que se le va a hacer la consulta
+	* @param str $condition => Recibe la condicion de la consulta, vale 0 cero sino se recibe el parametro
+	* @param str $groupby => Agrupa los registros y retorna la consulta, sino existe es cero
+	* @param str $orderby => Ordena los registros con valor (coluna-orden), si no existe es cero
+	* @param int $limit => Limite de registros a obtener, vale 0 cero si no se recibe el parametro
+	*/
+	public function getRows($table, $condition = 0, $groupby = 0, $orderby = 0 , $limit = 0){
+
+		sql = 'SELECT ';
+
+	}
+
+	
+	/**
+	* Realiza una consulta tipo ISERT en la Base de Datos
+	*
+	* INSERT
+    * [INTO] tbl_name [(col_name,...)]
+    * {VALUES | VALUE} ({expr | DEFAULT},...),(...),...
+    *
+    * @param str $table => nombre de la tabla a insertar
+    * @param array $values => Diccionario (clave-valor) contiene el nombre de la columna y el valor
+	*/
+	public function insertRow($table, $values){
+		sql = 'INSERT INTO ' . $table . ' ';
+
 
 	}
 	
